58f21ce43b545170b2144c59f88a599dc496305f
xuqiang <476556076@qq.com>
2018-05-07 17:12:52 +0800

    测试git拉取提交

--- boot-dubbo-consumer/src/main/java/com/xq/ConsumerApplication.java
+++ boot-dubbo-consumer/src/main/java/com/xq/ConsumerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @ImportResource({"classpath:config/spring-dubbo.xml"})
 public class ConsumerApplication {
+//    git代码测试
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
     }
